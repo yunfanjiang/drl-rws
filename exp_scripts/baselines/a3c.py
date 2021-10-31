bcb92c1a7c6084b3906971464055fb833e7ab466
Yunfan Jiang <yunfanjyf@gmail.com>
2021-10-31 15:01:31 -0700

    add a3c train script

---
+++ exp_scripts/baselines/a3c.py
@@ -0,0 +1,104 @@
+import os
+import argparse
+
+import ray
+from tqdm import tqdm
+import ray.rllib.agents.a3c as a3c
+
+import rps
+import rps.models
+from constants import POLICY_SCENARIO_MAP
+
+
+def train(args):
+    # parse bot policy
+    scenario_name = POLICY_SCENARIO_MAP[args.bot_policy]
+
+    # initialize ray and configuration
+    ray.init()
+    config = a3c.DEFAULT_CONFIG.copy()
+    config["framework"] = "torch"
+
+    # env related config
+    config["env_config"] = {
+        "scenario_name": scenario_name,
+        "state_obs": True,
+        "centralized_critic": False,
+        "seed": args.seed,
+    }
+
+    # computation resource related config
+    config["num_workers"] = args.num_workers
+    config["num_envs_per_worker"] = args.num_envs_per_worker
+    config["num_gpus"] = args.num_gpus
+
+    # model related config
+    config["model"] = {
+        "custom_model": "baseline_model",
+        "custom_model_config": {},
+    }
+
+    # hyper-parameters related config
+    config["lr"] = args.lr
+    config["gamma"] = args.gamma
+    config["entropy_coeff"] = args.entropy_coeff
+    config["train_batch_size"] = args.train_batch_size
+    config["rollout_fragment_length"] = args.rollout_fragment_length
+
+    # instantiate trainer
+    trainer = a3c.A3CTrainer(config=config, env="rws")
+
+    # start training
+    for _ in tqdm(range(int(args.train_steps))):
+        trainer.train()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("A2C RWS sanity check")
+
+    # env related config
+    parser.add_argument(
+        "--bot_policy",
+        type=str,
+        choices=["pure", "semi_pure", "pure_rock", "pure_paper", "pure_scissor"],
+        required=True,
+    )
+    parser.add_argument(
+        "--seed", type=int, default=0,
+    )
+
+    # computation resource related config
+    parser.add_argument(
+        "--num_workers", type=int, default=1,
+    )
+    parser.add_argument(
+        "--num_envs_per_worker", type=int, default=1,
+    )
+    parser.add_argument(
+        "--num_gpus", type=int, default=0, required=True,
+    )
+
+    # hyper-parameters
+    parser.add_argument(
+        "--lr", type=float, default=4e-4,
+    )
+    parser.add_argument(
+        "--gamma", type=float, default=0.99,
+    )
+    parser.add_argument(
+        "--entropy_coeff", type=float, default=0.003,
+    )
+    parser.add_argument(
+        "--train_batch_size", type=int, default=32,
+    )
+    parser.add_argument(
+        "--rollout_fragment_length", type=int, default=20,
+    )
+
+    # train steps
+    parser.add_argument(
+        "--train_steps", type=float, default=1e9,
+    )
+    args = parser.parse_args()
+
+    train(args)
--- rps/__init__.py
+++ rps/__init__.py
@@ -1,1 +1,10 @@
+from ray.tune.registry import register_env
+
 from .rps_env import RPSEnv
+
+
+def create_env(env_config):
+    return RPSEnv(**env_config)
+
+
+register_env("rws", create_env)
