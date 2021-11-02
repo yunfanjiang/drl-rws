daded76b3085848ebd1779448c6d0cc0e814b69c
Yunfan Jiang <yunfanjyf@gmail.com>
2021-11-01 18:41:16 -0700

    add no bot env

--- rps/__init__.py
+++ rps/__init__.py
@@ -1,10 +1,15 @@
 from ray.tune.registry import register_env
 
-from .rps_env import RPSEnv
+from .rps_env import RPSEnv, RPSEnvNoBot
 
 
 def create_env(env_config):
     return RPSEnv(**env_config)
 
 
+def create_env_no_bot(env_config):
+    return RPSEnv(**env_config)
+
+
 register_env("rws", create_env)
+register_env("rws_no_bot", create_env_no_bot)
--- rps/rps_env/__init__.py
+++ rps/rps_env/__init__.py
@@ -1,1 +1,1 @@
-from .env import RPSEnv
+from .env import RPSEnv, RPSEnvNoBot
